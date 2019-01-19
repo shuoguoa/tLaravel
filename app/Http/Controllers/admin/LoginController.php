930d34682b56b42d6f56cc96bd8265e16227bf0f
lfq <289397962@qq.com>
2019-01-19 23:12:37 +0800

    db连接

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+    public function getInfo(){
+        $users = DB::connection('mysql::read')->select('select * from member');
+        var_dump($users);exit;
+    }
 }
--- config/database.php
+++ config/database.php
@@ -42,11 +42,20 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'business_impoker'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'read' => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'business_impoker'),
+                'username' => env('DB_USERNAME', 'root'),
+                'password' => env('DB_PASSWORD', 'root'),
+            ],
+            'write' => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'impoker'),
+                'username' => env('DB_USERNAME', 'root'),
+                'password' => env('DB_PASSWORD', 'root'),
+            ],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
