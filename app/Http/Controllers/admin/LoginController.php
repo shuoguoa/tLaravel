8523178eb05774b38d6a1aae1bbdc07b4fa7cbe8
lfq <289397962@qq.com>
2019-01-20 13:48:22 +0800

    db配置

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class LoginController extends Controller
 {
     public function getInfo(){
-        $users = DB::connection('mysql::write')->select('select * from user');
+        $users = DB::connection('mysql::read')->select('select * from member');
         var_dump($users);exit;
     }
 }
