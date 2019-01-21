56dd5b514026d19d7d1d102ec4ede40ec059a4f1
lfq <289397962@qq.com>
2019-01-21 10:24:47 +0800

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-    <div class="container">
+    <div class="container" style="background-color: red">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
